b4a867a4ea38bf4583dce7b2ad6cc2780e48ba1f
Georgi Lyubenov <godzbanebane@gmail.com>
2023-10-04 16:24:49 +0300

    chore: factor out config parsing utils

--- lib/Config.hs
+++ lib/Config.hs
@@ -6,18 +6,20 @@ module Config (
   getConfig,
 ) where
 
+import Config.Common (
+  handleParseEnvResultMaybe,
+  handleParseEnvResultThrow,
+  readEnv,
+  readEnvText,
+ )
 import Control.Exception (throwIO)
 import Data.Aeson (FromJSON)
 import Data.Aeson qualified as Aeson
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
-import Data.Text qualified as T
 import Discord.Types qualified as DT
 import GHC.Generics (Generic)
-import System.Environment (lookupEnv)
-import Text.Printf (printf)
-import Text.Read (readEither)
-import Utils (whenLeft)
+import Utils (whenLeft, (<$$>))
 
 newtype AuthToken = MkAuthToken {get :: Text}
   deriving newtype (FromJSON)
@@ -32,7 +34,7 @@ data Config = Config
 
 getConfig :: Maybe FilePath -> IO Config
 getConfig configFile = do
-  token <- parseEnv (Right . MkAuthToken . T.pack) "AUTH_TOKEN"
+  token <- MkAuthToken <$$> readEnvText "AUTH_TOKEN"
   guildId <- readEnv "GUILD_ID"
   defaultRoles <- readEnv "DEFAULT_ROLES"
   let parseThrow x = do
@@ -56,43 +58,3 @@ getConfig configFile = do
           , guildId = fromMaybe configFromFile.guildId guildIdMay
           , defaultRoles = fromMaybe configFromFile.defaultRoles defaultRolesMay
           }
-
-data ParseEnvResult e a
-  = MissingKey String
-  | CouldNotParse {key :: String, rawValue :: String, err :: e}
-  | Success a
-
-parseEnv :: (String -> Either e a) -> String -> IO (ParseEnvResult e a)
-parseEnv parse key = do
-  maybeRawValue <- lookupEnv key
-  pure case maybeRawValue of
-    Nothing -> MissingKey key
-    Just rawValue ->
-      case parse rawValue of
-        Left err -> CouldNotParse key rawValue err
-        Right x -> Success x
-
-readEnv :: (Read a) => String -> IO (ParseEnvResult String a)
-readEnv = parseEnv readEither
-
--- | Throw on parse failures, Nothing on missing keys.
-handleParseEnvResultMaybe :: ParseEnvResult String a -> IO (Maybe a)
-handleParseEnvResultMaybe = \case
-  MissingKey _key ->
-    pure Nothing
-  CouldNotParse {key, rawValue, err} ->
-    throwIO . userError $ formatParseError key rawValue err
-  Success x -> pure $ Just x
-
--- | Throw on any errors.
-handleParseEnvResultThrow :: ParseEnvResult String a -> IO a
-handleParseEnvResultThrow = \case
-  MissingKey key ->
-    throwIO . userError $ printf "$%s not set" key
-  CouldNotParse {key, rawValue, err} ->
-    throwIO . userError $ formatParseError key rawValue err
-  Success x -> pure x
-
-formatParseError :: String -> String -> String -> String
-formatParseError key rawValue err =
-  printf "$%s=%s could not be parsed: %s" key rawValue err
